418afd77c51ce7c590861809aad728f865f5b03e
s-triar <34316314+s-triar@users.noreply.github.com>
2022-09-20 03:53:56 +0700

    gg

--- server/apppp.py
+++ server/apppp.py
@@ -11,25 +11,7 @@ from signalrcore.hub_connection_builder import HubConnectionBuilder
 URLSERVER="https://412d-140-0-40-217.ngrok.io"
 IDMINIPC=1
 slaves={}
-server_url = URLSERVER+"/DataParamSensorHub"
-handler = logging.StreamHandler()
-handler.setLevel(logging.DEBUG)
-hub_connection = HubConnectionBuilder()\
-            .with_url(server_url,options={"verify_ssl": False, "headers": {
-                    'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36",\
-                    'content-type':'application/json',\
-                }})\
-          .configure_logging(logging.DEBUG,socket_trace=True, handler=handler)\
-            .configure_logging(logging.DEBUG)\
-            .with_automatic_reconnect({
-                "type": "raw",
-                "keep_alive_interval": 10,
-                "reconnect_interval": 5,
-                "max_attempts": 5
-            }).build()
-hub_connection.on_open(lambda : [print("connection opened and handshake received ready to send messages"),hub_connection.send("JoinRoom", [str(IDMINIPC)+'-_-iot'])])
-hub_connection.on_close(lambda : print("connection closed"))
-hub_connection.start()
+
 
 sio = socketio.Server(async_mode="eventlet", cors_allowed_origins='*', engineio_logger=True)
 app = Flask(__name__)
@@ -80,7 +62,25 @@ def handle_message_event(msg,ggg):
 my_wsgi = socketio.WSGIApp(sio,app)
 app = socketio.Middleware(sio, my_wsgi)
 
-
+server_url = URLSERVER+"/DataParamSensorHub"
+handler = logging.StreamHandler()
+handler.setLevel(logging.DEBUG)
+hub_connection = HubConnectionBuilder()\
+            .with_url(server_url,options={"verify_ssl": False, "headers": {
+                    'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36",\
+                    'content-type':'application/json',\
+                }})\
+          .configure_logging(logging.DEBUG,socket_trace=True, handler=handler)\
+            .configure_logging(logging.DEBUG)\
+            .with_automatic_reconnect({
+                "type": "raw",
+                "keep_alive_interval": 10,
+                "reconnect_interval": 5,
+                "max_attempts": 5
+            }).build()
+hub_connection.on_open(lambda : [print("connection opened and handshake received ready to send messages"),hub_connection.send("JoinRoom", [str(IDMINIPC)+'-_-iot'])])
+hub_connection.on_close(lambda : print("connection closed"))
+hub_connection.start()
 
 eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
 
